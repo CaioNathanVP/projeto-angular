319db1e83afc726d9a726f93f9665fe02f120d50
Caio Nathan <142275972+CaioNathanVP@users.noreply.github.com>
2025-03-27 20:22:25 -0300

    modified:   frontend/src/app/app.component.css 	modified:   frontend/src/app/app.component.html 	modified:   frontend/src/app/app.component.spec.ts 	modified:   frontend/src/app/app.component.ts 	new file:   frontend/src/app/auth/user.model.ts 	new file:   frontend/src/app/messages/message.model.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+
+@Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [FormsModule, RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
+})
 
 export class AppComponent {
-  message = {
-    content: 'to ficano fera no assunto',
-    author: 'Caio Nathan'
-  }
-};
+    title = 'menssagem';
+    message = {
+        content: 'to ficano fera no assunto',
+        author: 'Caio Nathan'
+    };
+}
