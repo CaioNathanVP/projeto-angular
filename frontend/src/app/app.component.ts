157c8161144ae37ba209a4500ef3af1e7775faa2
Caio Nathan <142275972+CaioNathanVP@users.noreply.github.com>
2025-04-06 11:59:51 -0300

    modified:   frontend/src/app/app.component.html 	modified:   frontend/src/app/app.component.ts 	new file:   frontend/src/app/person.model.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
 import { FormsModule } from "@angular/forms";
 import { MessageComponent } from "./messages/message.component";
+import { Person } from "./person.model";
+
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     imports: [
-        FormsModule, 
-        RouterOutlet,
+        FormsModule,
         MessageComponent
     ],
     templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+    styleUrl: './app.component.css',  
+    
 })
 
 export class AppComponent {
@@ -21,4 +23,6 @@ export class AppComponent {
         content: 'to ficano fera no assunto',
         author: 'Caio Nathan'
     };
+    pessoa1: Person = new Person(100, 'Caio Nathan', 25);
+    pessoa3: Person = new Person();
 }
